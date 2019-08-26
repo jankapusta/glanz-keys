aceef1a6e7ebb7a6ee6671d6db2ec9127b5b76b7
Jan Kapusta <jan@clickclickdrive.net>
2019-08-26 15:35:51 +0200

    dev test

--- routes/dev.js
+++ routes/dev.js
@@ -6,26 +6,27 @@ router.get('/', function(req, res, next) {
   console.warn(req.headers);
   console.warn(req.cookies);
 
-  // res.send({
-  //   //h: req.headers,
-  //   //c: req.cookies,
-  //   //u: getUserName()
-  // });
-  res.send("I am fine");
+  res.send({
+    //h: req.headers,
+    //c: req.cookies,
+    //u: getUserName()
+    u:getUserName(req)
+  });
+  //res.send("I am fine");
 });
 
 module.exports = router;
 
-
-// const getUserName = () => {
+const getUserName = (req) => {
   
-//   const base64AuthData = 'Basic R2xhbno6Y2xlYW5JVA==';
-//   if(!base64AuthData) {
-//     return '';
-//   }
-//   const matches = base64AuthData.split('Basic ');
-//   let buff = Buffer.from(matches[1], 'base64');  
-//   let usernamePwdPair = buff.toString('utf-8');
-//   const usernamePwdArray = usernamePwdPair.split(':');
-//   return usernamePwdArray[0];
-// }
+  const base64AuthData = req.headers['Authorization'];
+  if(!base64AuthData) {
+    return '';
+  }
+  const matches = base64AuthData.split('Basic ');
+  let buff = Buffer.from(matches[1], 'base64');  
+  let usernamePwdPair = buff.toString('utf-8');
+  const usernamePwdArray = usernamePwdPair.split(':');
+  return usernamePwdArray[0];
+  
+}
