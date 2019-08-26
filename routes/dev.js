9773f71571bc008dadc7b72862a83060daae9ac4
Jan Kapusta <jan@clickclickdrive.net>
2019-08-26 14:57:11 +0200

    dev

--- routes/dev.js
+++ routes/dev.js
@@ -3,9 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.headers);
-  console.log(req.cookies);
-  res.send("I am fine");
+  console.warn(req.headers);
+  console.warn(req.cookies);
+  res.send({
+    h: req.headers,
+    c: req.cookies
+  });
+  //res.send("I am fine");
 });
 
 module.exports = router;
