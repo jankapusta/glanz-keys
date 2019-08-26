b639667875d6b58add66cb59d56de69ba747a8fc
Jan Kapusta <jan@clickclickdrive.net>
2019-08-26 12:45:55 +0200

    no index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { pageTitle: 'Glanz Berlin' });
+  res.status(404).send("Page not found!");
 });
 
 module.exports = router;
