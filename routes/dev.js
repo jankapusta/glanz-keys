e49f4fd429edaafc24ed027b9aaf8cc8eba70d1d
Jan Kapusta <jan@clickclickdrive.net>
2019-08-27 21:03:39 +0200

    qr codes for print

--- routes/dev.js
+++ routes/dev.js
@@ -15,7 +15,89 @@ router.get('/', function(req, res, next) {
   //res.send("I am fine");
 });
 
+
+router.get('/images', function(req, res, next) {
+
+  let images = [
+    '99d A.png',
+    '99d B.png',
+    'Arndt.png',
+    'Atlantik.png',
+    'Cashlife A.png',
+    'Cashlife B.png',
+    'Cuvry5 A.png',
+    'Cuvry5 B.png',
+    'Cuvry45 A.png',
+    'Cuvry45 B.png',
+    'Cuvry45 C.png',
+    'F16.png',
+    'Huuuge A.png',
+    'Huuuge B.png',
+    'Huuuge C.png',
+    'Huuuge D.png',
+    'Inva1.png',
+    'Inva2.png',
+    'Keith A.png',
+    'Keith B.png',
+    'Linkstr..png',
+    'Mediamath black A.png',
+    'Mediamath black B.png',
+    'Mediamath black C.png',
+    'Mediamath black D.png',
+    'Mediamath stairs.png',
+    'Mediamath white A.png',
+    'Mediamath white B.png',
+    'Mediamath white C.png',
+    'Mediamath white D.png',
+    'Mollstrase C.png',
+    'Mollstrasse A.png',
+    'Mollstrasse D.png',
+    'Mollstrasse E.png',
+    'Mollstrasse F.png',
+    'Mollstrasse G.png',
+    'Mollsttrasse B.png',
+    'Peterburgerstr.png',
+    'Rheinsberger.png',
+    'Riot A.png',
+    'Riot B.png',
+    'Riot C.png',
+    'Riot D.png',
+    'Strausberger.png',
+    'Vantik A.png',
+    'Vantik B.png',
+    'Weinsberg.png',
+    'Wuhlisch.png',
+    ];
+
+
+    let x = images.map((value, inex) => {
+      return {
+        'filename': encodeURIComponent(value),
+        'short': parseOfficeName(value),
+      };
+
+    });
+
+    res.render('qr-codes', {
+        images: x
+    });
+
+
+});
+
 module.exports = router;
+
+const parseOfficeName = (filename) => {
+  let parts = filename.split('.');
+  let fullName = parts[0];
+  let extraLength = fullName.length - 15;
+  if(extraLength > 0) {
+    firstSpaceIndex = fullName.indexOf(' ');
+    let shortName = fullName.substr(0, firstSpaceIndex - extraLength - 2) + '.' + fullName.substr(firstSpaceIndex, fullName.length);
+    return shortName;
+  }
+  return fullName;
+}
 
 const getUserName = (req) => {
   
