8adce5c633e683c51b483e38582215eb64793bb0
Jan Kapusta <jan@clickclickdrive.net>
2019-10-20 16:01:36 +0200

    simplify username function

--- functions/getUserName.js
+++ functions/getUserName.js
@@ -6,14 +6,17 @@ module.exports =  (req) => {
   if(process.env.ADMIN_OPEN) {
     return 'Admin';
   }
-  const base64AuthData = req.headers['authorization'] || req.headers['Authorization'];
-  if(!base64AuthData) {
-    return '';
-  }
-  const matches = base64AuthData.split('Basic ');
-  let buff = Buffer.from(matches[1], 'base64');  
-  let usernamePwdPair = buff.toString('utf-8');
-  const usernamePwdArray = usernamePwdPair.split(':');
-  return usernamePwdArray[0] === LOW_PERMISSION_USER ? false : usernamePwdArray[0];
+
+  return req.auth.user;
+
+  // const base64AuthData = req.headers['authorization'] || req.headers['Authorization'];
+  // if(!base64AuthData) {
+  //   return '';
+  // }
+  // const matches = base64AuthData.split('Basic ');
+  // let buff = Buffer.from(matches[1], 'base64');  
+  // let usernamePwdPair = buff.toString('utf-8');
+  // const usernamePwdArray = usernamePwdPair.split(':');
+  // return usernamePwdArray[0] === LOW_PERMISSION_USER ? false : usernamePwdArray[0];
 
 }
