ede333e634783e1ce9a000ba25829a4340ac583e
Jan Kapusta <jan@clickclickdrive.net>
2020-04-18 18:30:33 +0200

    add homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(404).send("Page not found!");
+  res.render('index', { 
+    title: 'Cleaning offices',
+  });
 });
 
 module.exports = router;
