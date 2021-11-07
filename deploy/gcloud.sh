17ac0158aa12ec3146dcb0712318e09e65dbec32
jankapusta <jan.kapusta@gmail.com>
2021-11-07 23:12:46 +0100

    shared mongo

--- deploy/gcloud.sh
+++ deploy/gcloud.sh
@@ -9,7 +9,7 @@ BASE_NAME=keys
 NAMESPACE=glanz-production
 
 GLANZ_MONGO_PORT=27017
-GLANZ_MONGO_HOST=mongo
+GLANZ_MONGO_HOST=mongo.storage
 
 LAST_COMMIT=`git rev-parse --short HEAD`
 PORT=3000
@@ -21,18 +21,8 @@ CONTAINER_IMAGE=gcr.io/${PROJECT}/glanz-${ENVIRONMENT}-${BASE_NAME}:v${LAST_COMM
 docker build --build-arg node_env=${NODE_ENV} -t ${CONTAINER_IMAGE} --platform linux/x86_64 .
 docker push ${CONTAINER_IMAGE}
 
-IF=./deploy/kube.db.yaml
-OF=./deploy/kube.db-ready.yaml
-eval "echo \"$(cat ${IF})\" > ${OF}"
-
-kubectl apply --record --namespace=${NAMESPACE} -f ${OF}
-rm ${OF}
-
 IF=./deploy/kube.app.yaml
 OF=./deploy/kube.app-ready.yaml
 eval "echo \"$(cat ${IF})\" > ${OF}"
-
-
-
 kubectl apply --record --namespace=${NAMESPACE} -f ${OF}
 rm ${OF}
