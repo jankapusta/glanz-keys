361b0c13d871adcfa94eecaf4f02ef13db122f8b
jankapusta <jan.kapusta@gmail.com>
2021-11-07 18:42:54 +0100

    use yarn

--- deploy/local.sh
+++ deploy/local.sh
@@ -1,4 +1,6 @@
-mongod --config /usr/local/etc/mongod.conf
+brew services start mongodb-community@5.0
+#mongod --config /opt/homebrew/etc/mongod.conf
+#docker run --name some-mongo -d mongo
 export MONGO_HOST=127.0.0.1
 export ADMIN_OPEN=1
-npm run devstart
+yarn run devstart
